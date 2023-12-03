a78e5e454460a1ffe14b3a967501e639d84edab8
shenny85 <shenderson144@ivytech.edu>
2023-12-03 13:58:12 -0500

    added review header and cleaned up service cards

--- Final-Project-1130-main/src/Pages/HomeWebPage.js
+++ Final-Project-1130-main/src/Pages/HomeWebPage.js
@@ -1,6 +1,7 @@
 import Cards from '../Cards/Cards'
 import Navbar from './Navbar'
 import './Navbar.css';
+import './HomeWebPage.css'
 
 const HomePage = () => {
     return(
@@ -23,7 +24,7 @@ const HomePage = () => {
          <div className="container fs-2 p-3 fw-bold border border-black text-center">
     Welcome to IndyFIXER</div>
  
-    <div className="container p-3 fw-bold border border-white text-center">
+    <div className="container p-3 fw-bold border text-center">
       <p className="fs-3 p-5 fw-bold">At IndyFixer, our mission is to provide exceptional services to our valued customers and communities. 
       We are dedicated to delivering quality workmanship, reliable solutions, and outstanding customer service. 
       With our team of skilled professionals, we always aim to exceed our customers' expectations, tackling each 
@@ -38,7 +39,7 @@ const HomePage = () => {
   </div>
 
   <div className="container align-items-lg-stretch">
-    <div className="container row bg-white p-4 justify-content-around">
+    <div className="container row bg-white-subtle p-4 justify-content-around">
    
       <div className="col-sm col mb-4">
         <div className="card  h-100" style={{width: 250}}>
@@ -47,8 +48,8 @@ const HomePage = () => {
             <h5 className="card-title">Interior Design</h5>
             <p className="card-text"> Luxury, innovative solutions, curated in unique styles to fit your needs. We provide
               support with all phases of the design process.
-            </p>
-            <a href="LinksToAllJobs/InteriorDesign.html" className="btn btn-primary">Learn More</a>
+           
+            <a href="LinksToAllJobs/InteriorDesign.html" className="btn btn-primary mt-3">Learn More</a> </p>
           </div>
         </div>
       </div>
@@ -58,8 +59,8 @@ const HomePage = () => {
           <img src="https://i.postimg.cc/L6q2rCwN/TV-mount.jpg" className="card-img-top" alt="..." width="150px" height="150px"/>
           <div className="card-body border primary">
             <h5 className="card-title">Mounting</h5>
-            <p className="card-text">Offering fast and accurate mounting services. We can handle basic and custom mounting to beautify and declutter your space.</p>
-            <a href="LinksToAllJobs/Mounting.html" className="btn btn-primary">Learn More</a>
+            <p className="card-text">Offering fast and accurate mounting services. We can handle basic and custom mounting to beautify and declutter your space.
+            <a href="LinksToAllJobs/Mounting.html" className="btn btn-primary mt-3">Learn More</a></p>
           </div>
         </div>
       </div>
@@ -69,8 +70,8 @@ const HomePage = () => {
           <img src="https://i.postimg.cc/4y7qFMyc/plumbing.jpg" className="card-img-top" alt="..." width="150px" height="150px"/>
           <div className="card-body border primary">
             <h5 className="card-title">Plumbing</h5>
-            <p className="card-text">Offering full service repair and maintenance services. We help you develop the best plan for your plumbing or drainage needs.</p>
-            <a href="LinksToAllJobs/Plumbering.html" className="btn btn-primary">Learn More</a>
+            <p className="card-text">Offering full service repair and maintenance services. We help you develop the best plan for your plumbing or drainage needs.
+            <a href="LinksToAllJobs/Plumbering.html" className="btn btn-primary mt-3">Learn More</a></p>
           </div>
         </div>
       </div>
@@ -80,8 +81,8 @@ const HomePage = () => {
           <img src="https://i.postimg.cc/BZFGpLKV/Landscape.jpg" className="card-img-top" alt="..." width="150px" height="150px"/>
           <div className="card-body border primary">
             <h5 className="card-title">Landscaping</h5>
-            <p className="card-text">From weekly mowing to custom landscapes we are ready to get your project off the ground. Let us enhance your curb appeal.</p>
-            <a href="LinksToAllJobs/Landscaping.html" className="btn btn-primary">Learn More</a>
+            <p className="card-text">From weekly mowing to custom landscapes we are ready to get your project off the ground. Let us enhance your curb appeal.
+            <a href="LinksToAllJobs/Landscaping.html" className="btn btn-primary mt-3">Learn More</a></p>
           </div>
         </div>
       </div>
@@ -91,8 +92,8 @@ const HomePage = () => {
           <img src="https://i.postimg.cc/bwCMppnn/Housekeeping.jpg" className="card-img-top" alt="..." width="150px" height="150px"/>
           <div className="card-body border primary">
             <h5 className="card-title">Housekeeping</h5>
-            <p className="card-text">Providing solutions for residential and commercial cleaning needs. Give us a try and we'll get the job done right the first time.</p>
-            <a href="LinksToAllJobs/HouseKeeping.html" className="btn btn-primary">Learn More</a>
+            <p className="card-text">Providing solutions for residential and commercial cleaning needs. Give us a try and we'll get the job done right the first time.
+            <a href="LinksToAllJobs/HouseKeeping.html" className="btn btn-primary mt-3">Learn More</a></p>
           </div>
         </div>
       </div>
@@ -102,8 +103,8 @@ const HomePage = () => {
           <img src="https://i.postimg.cc/9Mtrw8CG/Flooring.jpg" className="card-img-top" alt="..." width="150px" height="150px"/>
           <div className="card-body border primary">
             <h5 className="card-title">Flooring</h5>
-            <p className="card-text">Providing solutions for residential and commercial flooring needs. We've got you covered for complete repairs and installs.</p>
-            <a href="LinksToAllJobs/Flooring.html" className="btn btn-primary">Learn More</a>
+            <p className="card-text">Providing solutions for residential and commercial flooring needs. We've got you covered for complete repairs and installs.
+            <a href="LinksToAllJobs/Flooring.html" className="btn btn-primary mt-3">Learn More</a></p>
           </div>
         </div>
       </div>
@@ -113,8 +114,8 @@ const HomePage = () => {
           <img src="https://i.postimg.cc/brhV3v77/images-1.jpg" className="card-img-top" alt="..." width="150px" height="150px"/>
           <div className="card-body border primary">
             <h5 className="card-title">Pool Maintenance</h5>
-            <p className="card-text">We provide testing and maintenance services to maintain your private spa and keep you swimming as long as the weather allows.  </p>
-            <a href="LinksToAllJobs/PoolMaint.html" className="btn btn-primary">Learn More</a>
+            <p className="card-text">We provide testing and maintenance services to maintain your private spa and keep you swimming as long as the weather allows. 
+            <a href="LinksToAllJobs/PoolMaint.html" className="btn btn-primary mt-3">Learn More</a></p>
           </div>
         </div>
       </div>
@@ -124,15 +125,18 @@ const HomePage = () => {
           <img src="https://i.postimg.cc/hvcMV5xg/Home-Security.jpg" className="card-img-top" alt="..." width="150px" height="150px"/>
           <div className="card-body border primary">
             <h5 className="card-title">Home Security</h5>
-            <p className="card-text">We provide initial consulting services to get you the right protection for your home when you need it; then we install it for you.</p>
-            <a href="LinksToAllJobs/HomeSecurity.html" className="btn btn-primary">Learn More</a>
+            <p className="card-text">We provide initial consulting services to get you the right protection for your home when you need it; then we install it for you.
+            <a href="LinksToAllJobs/HomeSecurity.html" className="btn btn-primary mt-3">Learn More</a></p>
           </div>
         </div>
       </div>
     </div>
   </div>
 
-  <div className="row align-items-center">
+  <div className="container border border-black">
+    <div className=" text-center row fs-2 p-3"><strong>Reviews</strong></div>
+  </div>
+  <div className="row align-items-center fs-3">
     <Cards />
 </div>
 
