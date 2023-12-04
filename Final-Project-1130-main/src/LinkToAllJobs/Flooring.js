d6737ea154fc43b2a44ea51d8bba5aa56bc8b4be
shenny85 <shenderson144@ivytech.edu>
2023-12-03 22:17:15 -0500

    added pictures

--- Final-Project-1130-main/src/LinkToAllJobs/Flooring.js
+++ Final-Project-1130-main/src/LinkToAllJobs/Flooring.js
@@ -28,7 +28,7 @@ const Flooring = () => {
     <div className="container row bg-white-subtle p-4 justify-content-around">
         <div className="col-sm col mb-5">  
         <div className="card h-100" style={{width: 225}}>
-          <img src="https://i.postimg.cc/9Mtrw8CG/Flooring.jpg" className="card-img-top" alt="..." width="150px" height="150px"/>
+          <img src="https://i.postimg.cc/nMf9g5wT/vinyl.jpg" className="card-img-top" alt="..." width="150px" height="150px"/>
           <div className="card-body border primary">
           <h5 className="card-title">Vinyl</h5>
             <h5 className="card-title">
@@ -41,7 +41,7 @@ const Flooring = () => {
 
           <div className="col-sm col mb-5"> 
           <div className="card h-100" style={{width: 225}}>
-          <img src="https://i.postimg.cc/9Mtrw8CG/Flooring.jpg" className="card-img-top" alt="..." width="150px" height="150px"/>
+          <img src="https://i.postimg.cc/HkB9Kwb5/Hardwood-flooring.jpg" className="card-img-top" alt="..." width="150px" height="150px"/>
           <div className="card-body border primary">
           <h5 className="card-title">Wood</h5>
             <h5 className="card-title">
@@ -54,7 +54,7 @@ const Flooring = () => {
          
           <div className="col-sm col mb-5"> 
           <div className="card h-100" style={{width: 225}}>
-          <img src="https://i.postimg.cc/9Mtrw8CG/Flooring.jpg" className="card-img-top" alt="..." width="150px" height="150px"/>
+          <img src="https://i.postimg.cc/X7nFbZjg/laminate.jpg" className="card-img-top" alt="..." width="150px" height="150px"/>
           <div className="card-body border primary">
           <h5 className="card-title">Laminate</h5>
             <h5 className="card-title">
@@ -67,7 +67,7 @@ const Flooring = () => {
         
           <div className="col-sm col mb-5"> 
           <div className="card h-100" style={{width: 225}}>
-          <img src="https://i.postimg.cc/9Mtrw8CG/Flooring.jpg" className="card-img-top" alt="..." width="150px" height="150px"/>
+          <img src="https://i.postimg.cc/3xVddWTV/carpet.jpg" className="card-img-top" alt="..." width="150px" height="150px"/>
           <div className="card-body border primary">
           <h5 className="card-title">Carpet</h5>
             <h5 className="card-title">
@@ -80,7 +80,7 @@ const Flooring = () => {
       
           <div className="col-sm col mb-5"> 
           <div className="card h-100" style={{width: 225}}>
-          <img src="https://i.postimg.cc/9Mtrw8CG/Flooring.jpg" className="card-img-top" alt="..." width="150px" height="150px"/>
+          <img src="https://i.postimg.cc/RZ6059HR/tile.jpg" className="card-img-top" alt="..." width="150px" height="150px"/>
           <div className="card-body border primary">
           <h5 className="card-title">Tile</h5>
             <h5 className="card-title">
